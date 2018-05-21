c28a72339b56dda5d227fdbbd73da0ae60680bab
sean phillips <sean-phillips@live.com>
2018-05-20 20:17:28 -0400

    Initial Commit. Started work on GetGimpExeLocation.cs.

---
+++ CreateURealmsTilesV2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CreateURealmsTilesV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_gimpLocation = new System.Windows.Forms.TextBox();
+            this.label_gimpLocation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox_gimpLocation.Location = new System.Drawing.Point(109, 60);
+            this.textBox_gimpLocation.Name = "textBox1";
+            this.textBox_gimpLocation.Size = new System.Drawing.Size(268, 20);
+            this.textBox_gimpLocation.TabIndex = 0;
+            this.textBox_gimpLocation.Text = GetGimpExeLocation.GimpExeLocation();
+            this.textBox_gimpLocation.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label_gimpLocation.AutoSize = true;
+            this.label_gimpLocation.Location = new System.Drawing.Point(25, 63);
+            this.label_gimpLocation.Name = "label1";
+            this.label_gimpLocation.Size = new System.Drawing.Size(78, 13);
+            this.label_gimpLocation.TabIndex = 1;
+            this.label_gimpLocation.Text = "GIMP Location";
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_gimpLocation);
+            this.Controls.Add(this.textBox_gimpLocation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_gimpLocation;
+        private System.Windows.Forms.Label label_gimpLocation;
+    }
+}
+
---
+++ CreateURealmsTilesV2/GetGimpExeLocation.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace CreateURealmsTilesV2
+{
+    class GetGimpExeLocation
+    {
+        public static string GimpExeLocation()
+        {
+            string gimpLocation = @"C:\Program Files\GIMP 2\bin\gimp-2.8.exe";
+
+            if (File.Exists(gimpLocation) == false)
+            {
+                OpenFileDialog GetGIMPExe = new OpenFileDialog
+                {
+
+                    Title = "'gimp-2.8.exe' not found in default location. Please select it.",
+                    InitialDirectory = @"C:\Program Files\GIMP 2\bin",
+                    Filter = "gimp-2.8.exe|gimp-2.8.exe",
+                    FilterIndex = 2,
+                    RestoreDirectory = true
+
+                };
+                GetGIMPExe.ShowDialog();
+                gimpLocation = GetGIMPExe.FileName;
+            }
+
+            return gimpLocation;
+
+        }
+
+
+    }
+}
