be7266c1daea973241d1de81d5e00ef5946f12d8
sean phillips <sean-phillips@live.com>
2018-05-22 00:09:08 -0400

    Added GetImagesLocation.cs functionality and buttons.

--- CreateURealmsTilesV2/Form1.Designer.cs
+++ CreateURealmsTilesV2/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace CreateURealmsTilesV2
+﻿using System;
+namespace CreateURealmsTilesV2
 {
     partial class Form1
     {
@@ -30,25 +31,44 @@
         {
             this.textBox_gimpLocation = new System.Windows.Forms.TextBox();
             this.label_gimpLocation = new System.Windows.Forms.Label();
+            this.textBox_imageLocation = new System.Windows.Forms.TextBox();
+            this.button_getImage = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox_gimpLocation
             // 
             this.textBox_gimpLocation.Location = new System.Drawing.Point(109, 60);
-            this.textBox_gimpLocation.Name = "textBox1";
+            this.textBox_gimpLocation.Name = "textBox_gimpLocation";
+            this.textBox_gimpLocation.ReadOnly = true;
             this.textBox_gimpLocation.Size = new System.Drawing.Size(268, 20);
             this.textBox_gimpLocation.TabIndex = 0;
-            this.textBox_gimpLocation.Text = GetGimpExeLocation.GimpExeLocation();
-            this.textBox_gimpLocation.ReadOnly = true;
             // 
-            // label1
+            // label_gimpLocation
             // 
             this.label_gimpLocation.AutoSize = true;
             this.label_gimpLocation.Location = new System.Drawing.Point(25, 63);
-            this.label_gimpLocation.Name = "label1";
+            this.label_gimpLocation.Name = "label_gimpLocation";
             this.label_gimpLocation.Size = new System.Drawing.Size(78, 13);
             this.label_gimpLocation.TabIndex = 1;
             this.label_gimpLocation.Text = "GIMP Location";
+            // 
+            // textBox_imageLocation
+            // 
+            this.textBox_imageLocation.Location = new System.Drawing.Point(109, 98);
+            this.textBox_imageLocation.Name = "textBox_imageLocation";
+            this.textBox_imageLocation.ReadOnly = true;
+            this.textBox_imageLocation.Size = new System.Drawing.Size(268, 20);
+            this.textBox_imageLocation.TabIndex = 2;
+            // 
+            // button_getImage
+            // 
+            this.button_getImage.Location = new System.Drawing.Point(28, 96);
+            this.button_getImage.Name = "button_getImage";
+            this.button_getImage.Size = new System.Drawing.Size(75, 23);
+            this.button_getImage.TabIndex = 4;
+            this.button_getImage.Text = "Get Image";
+            this.button_getImage.UseVisualStyleBackColor = true;
+            this.button_getImage.Click += new EventHandler(button_getImage_Click);
 
             // 
             // Form1
@@ -56,6 +76,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_getImage);
+            this.Controls.Add(this.textBox_imageLocation);
             this.Controls.Add(this.label_gimpLocation);
             this.Controls.Add(this.textBox_gimpLocation);
             this.Name = "Form1";
@@ -65,10 +87,18 @@
 
         }
 
+        void button_getImage_Click(object sender, EventArgs e)
+        {
+            string[] results = GetImagesLocation.ImagesLocation();
+            textBox_imageLocation.Text = String.Join(", ", results);
+        }
+
         #endregion
 
         private System.Windows.Forms.TextBox textBox_gimpLocation;
         private System.Windows.Forms.Label label_gimpLocation;
+        private System.Windows.Forms.TextBox textBox_imageLocation;
+        private System.Windows.Forms.Button button_getImage;
     }
 }
 
---
+++ CreateURealmsTilesV2/GetImagesLocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CreateURealmsTilesV2
+{
+    class GetImagesLocation
+    {
+        public static string[] ImagesLocation()
+        {
+            
+            OpenFileDialog GetImage = new OpenFileDialog
+            {
+
+                Title = "Please select an image(s).",
+                InitialDirectory = @"C:\Users\"+ Environment.UserName + @"\Desktop",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                Multiselect = true
+
+            };
+            GetImage.ShowDialog();
+
+            return GetImage.FileNames;
+
+        }
+    }
+}
